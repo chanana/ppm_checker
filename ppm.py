9977b302b2c15cd2fcc0c3aabda00ce2d16a88dc
Shaurya Chanana <schanana@wisc.edu>
2018-05-02 13:12:57 -0500

    Changed format of input

--- ppm.py
+++ ppm.py
@@ -1,12 +1,27 @@
-#PPM plus minus calculator
-def plus_minus(ref, ppm):
-    pls = ref * (1 + (ppm * 1e-6))
-    mis = ref * (1 - (ppm * 1e-6))
-    return(pls, mis)
+#Returns the mass range given a ppm and a reference mass
+def plus_minus(reference, ppm):
+    mass_upper = reference * (1 + (ppm * 1e-6))
+    mass_lower = reference * (1 - (ppm * 1e-6))
+    return(mass_lower, mass_upper)
 
-while True:
-    ref, ppm = [float(x) for x in raw_input("Enter mass <space> PPM: ").split()]
-    print(plus_minus(ref, ppm))
-    i = raw_input("Press any key to continue or ENTER to exit")
-    if not i:
-        break
+# Checks if string is float or not
+def isFloat(number):
+    try:
+        float(number)
+        return True
+    except ValueError:
+        return False
+
+# Keeps running until user enters 'q'
+i = 'a' # just an initial value of i
+while i != 'q':
+    try:
+        ref, ppm = raw_input("\nEnter mass <space> ppm: ").split()
+    except ValueError:
+        print("\nTry entering TWO values\n")
+        continue
+    if isFloat(ref) and isFloat(ppm): # if they are both floats,
+        print(plus_minus(float(ref), float(ppm)))
+    else:
+        print("\nPlease enter a number! -_- \n")
+    i = raw_input("\nEnter 'q' to quit or anything else to continue: ")
